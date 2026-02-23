e2644ee8d169279bb4601cdadfd87f4df41ad0f1
Majoriti <hello@majoriti.world>
2026-02-23 21:56:41 +0200

    Remove report.html file, eliminating outdated content and associated styles to streamline the project.

--- src/main.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import NetworkGraph from './components/NetworkGraph.jsx'
-
-const rootEl = document.getElementById('graph-root')
-if (rootEl) {
-  ReactDOM.createRoot(rootEl).render(<NetworkGraph />)
-}
